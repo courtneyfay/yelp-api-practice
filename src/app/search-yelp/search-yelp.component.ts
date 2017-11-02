18649ccd690ba011ca6387a80eb67744a1500f75
Courtney Fay <fay.courtney@gmail.com>
2017-11-02 15:42:13 -0600

    starting to work on api call and authentication

--- src/app/search-yelp/search-yelp.component.ts
+++ src/app/search-yelp/search-yelp.component.ts
@@ -16,10 +16,21 @@ export class SearchYelpComponent implements OnInit {
   ngOnInit() {
   }
 
-  findRestaurant(restaurantName){
-    console.log('finding ' + restaurantName);
-    this.http.get('https://swapi.co/api/people/?search=' + restaurantName)
+  findRestaurants(cityName){
+    console.log('finding ' + cityName);
+    this.http.get('https://api.yelp.com/v2/search?term=food&location=' + cityName)
 	    .toPromise()
 	    .then(response => console.log(response.json()));
 	}
+	/*
+	https://www.yelp.com/developers/documentation/v3 (v2 is being deprecated next summer 2018 and is no longer accepting new signups)
+	https://www.yelp.com/developers/documentation/v3/authentication
+	currently working on oauth authentication
+	Definitely do!
+		Make Yelp’s logo prominently visible on all content that originated from Yelp. The casual user should always know where Yelp content comes from!
+		Make the Yelp logo link to yelp.com or the business’s Yelp page wherever and whenever you use it.
+		Link directly to the corresponding Yelp pages whenever you display star ratings, review excerpts, and review counts from Yelp. Please don’t link content from Yelp to another page, interstitial, or any other location other than directly back to Yelp!
+		Display aggregate star ratings side-by-side with the number of reviews on which they’re based.
+		Include “read more” links to full reviews whenever you use review excerpts. Otherwise, include a “Read Reviews” link to the corresponding business pages on Yelp.
+	*/
 }
